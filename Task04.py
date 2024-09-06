001bad413671d70df155e5ed9cb8d783d9e4099d
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-06 12:58:41 +0300

    Поправил задание 4

--- Task04.py
+++ Task04.py
@@ -6,18 +6,22 @@ class GameScore:
     def __init__(self):
         self._current_score = 0
 
-    def add(self, score: int):
+    def add_score(self, score: int):
         if score > 0:
             if score > 1000:
                 raise ScoreLimitExceededError
             self._current_score += score
 
-    def sub(self, score: int):
-        if self._current_score - score > 0:
-            self._current_score -= score
+    def subtract_score(self, score: int):
+        if not self._current_score - score > 0:
+            raise ValueError("При снижении очков счет становится отрицательным")
+        self._current_score -= score
 
-
-game_score = GameScore()
-game_score.add(10)
-game_score.add(1110)
-
+try:
+    game_score = GameScore()
+    game_score.add_score(10)
+    game_score.add_score(1110)
+except ValueError as e:
+    print(e)
+except ScoreLimitExceededError as e:
+    print(e)
